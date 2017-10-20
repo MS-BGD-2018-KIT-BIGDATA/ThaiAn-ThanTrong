7c2031a3a45bd4ab04db30a9485759e024a1a8e9
thaiantt <thaian.tt@gmail.com>
2017-10-20 14:51:36 +0200

    Added exo_cc_lesson_04

--- Lesson4/exo_cc_lesson_04.py
+++ Lesson4/exo_cc_lesson_04.py
@@ -1,104 +1,96 @@
-## Generate a datafile with used Renault Zoe cars prices in IdF, PACA and Aquitaine
-## File must contain :
-## Car Version | Year | kms | Price | Telephone | Professional / Person
+# Labo | Qté principe actif | Année commercialisation | Mois | Prix | Restriction age | Restriction poids
 
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 import json
+import re
 import numpy as np
 import os
-import time
-import re
 
+api = "https://open-medicaments.fr/api/v1/medicaments"
 
-# IdF
-url_idf = 'https://www.leboncoin.fr/voitures/offres/ile_de_france/?brd=Renault&mdl=Zoe&f=p'
-
-max_rank = 5
-
-versions = r'life|intens|zen'
 
 # Using BeautifulSoup
 def getSoupFromURL(url, method='get', data={}):
-    
     if method == 'get':
         res = requests.get(url)
     elif method == 'post':
         res = requests.post(url, data=data)
     else:
         return None
-    
+
     if res.status_code == 200:
         soup = BeautifulSoup(res.text, 'html.parser')
         return soup
     else:
         return None
 
-def getAllLinks(url):
-    soup = getSoupFromURL(url)
-    
-    if soup:
-        all_cars = soup.find_all("a", class_="list_item clearfix trackable")
-        links = ["http:" + car['href'] for car in all_cars]
+def getInfoMed():
 
-    return links
+    api_url = api + "?query=ibuprof%C3%A8ne"
+    res = requests.get(api_url)
+    assert res.status_code == 200
+    all = json.loads(res.text)
 
-def getInfoCar(link):
-    soup = getSoupFromURL(link)
+    list = []
 
-    car_info = {}
-    classes = ['item_price clearfix', 'clearfix']
-    if soup:
-        
-        ### COLLECT DATA ###
-        # Properties - Values
-        properties = list(map(lambda x: x.text.strip().lower(), soup.find_all("span", class_='property')))
-        values = list(map(lambda x: x.text.strip().lower(), soup.find_all("span", class_='value')))
-        
-        car_info = {property : value for property, value in zip(properties, values)}
-        
-        # Version #
-        description = soup.find_all('p', class_='value')[0].text.strip().lower()
-        s = re.search(versions, description)
-        if s :
-            version = s.group(0)
+    for element in all:
+        id = element['codeCIS']
+
+        url_med = api + "/" + id
+        res = requests.get(url_med)
+        assert res.status_code == 200
+        info_med = json.loads(res.text)
+
+        cols = {}
+
+        # LABO
+        cols['Labo'] = info_med['titulaires'][0]
+
+        # PRINCIPE ACTIF
+        s_mg = re.search("(\d{1,3}) mg", info_med['compositions'][0]['substancesActives'][0]['dosageSubstance'])
+        if s_mg is not None: # convert mg to g
+            qty = float(s_mg.group(1)) / 10
         else:
-            version = 'Unknown'
-        car_info['version'] = version
-        
-        ### CLEANING ###
-        # Remove currency sign
-        car_info['prix'] = float(car_info['prix'].replace('\xa0€', '').replace(' ', ''))
-        
-        # Km
-        car_info['kilométrage'] = float(car_info['kilométrage'].replace('km', '').replace(' ', ''))
-        
-        # Year
-        car_info['année-modèle'] = int(car_info['année-modèle'])
+            qty = float(info_med['compositions'][0]['substancesActives'][0]['dosageSubstance'].split()[0])
 
-        ### FIND SELLER ###
-        seller = soup.find_all("p", class_="title")[0].text.strip()
-        car_info['vendeur'] = seller
-        
-        return car_info
+        cols['Qte principe actif (g)'] = qty
 
-def buildDataFrame(list_cars):
-    df = pd.DataFrame(list_cars)
-    print(df.dtypes)
+        # ANNEE COMMERCIALISATION
+        cols['Annee commercialisation'] = info_med['presentations'][0]["dateDeclarationCommercialisation"].split("-")[0]
+
+        # MOIS COMMERCIALISATION
+        cols['Mois commercialisation'] = info_med['presentations'][0]["dateDeclarationCommercialisation"].split("-")[1]
+
+        # PRIX
+        cols['Prix'] = info_med['presentations'][0]["prix"]
+
+        # RESTRICTION AGE
+        s = re.search("(\d{1,2}) an", info_med['indicationsTherapeutiques'])
+        if s is not None:
+            cols['Restriction age'] = s.group(1)
+        else :
+            cols['Restriction age'] = np.NaN
+
+        # RESTRICTION AGE
+        s = re.search("(\d{1,2}) kg", info_med['indicationsTherapeutiques'])
+        if s is not None:
+            cols['Restriction poids (kg)'] = s.group(1)
+        else:
+            cols['Restriction poids (kg)'] = np.NaN
+
+        list.append(cols)
+
+    df = pd.DataFrame(list, columns=['Labo', 'Qte principe actif (g)', 'Annee commercialisation', 'Mois commercialisation', 'Restriction age', 'Restriction poids (kg)'])
     return df
+
 
 # Main
 if __name__ == "__main__":
-    start_time = time.time()
-    
-    links = getAllLinks(url_idf)
-    
-    list_cars = [getInfoCar(link) for link in links]
-    
-    df = buildDataFrame(list_cars)
+    df = getInfoMed()
     print(df)
 
-    print("\nTime elapsed to get avg stargazer per user : {} s".format(time.time() - start_time))
-
-
+    if os.path.isfile("open_medicaments.csv"):
+        os.remove("open_medicaments.csv")
+    df.to_csv("open_medicaments.csv", sep=";")
