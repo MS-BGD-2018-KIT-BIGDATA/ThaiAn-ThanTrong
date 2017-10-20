8d5ced33c63d0643688164207d4b40a3479d23a8
thaiantt <thaian.tt@gmail.com>
2017-10-20 14:54:49 +0200

    Updated version with file from other git repositorye

--- Lesson4/exo_dom_lesson_04.py
+++ Lesson4/exo_dom_lesson_04.py
@@ -10,11 +10,11 @@ import numpy as np
 import os
 import time
 import re
+import itertools
 
-
-# IdF
-url_idf = 'https://www.leboncoin.fr/voitures/offres/ile_de_france/?brd=Renault&mdl=Zoe&f=p'
-
+# Regions
+regions = ['ile_de_france', 'paca', 'acquitaine']
+url = 'https://www.leboncoin.fr/voitures/offres/{}/?brd=Renault&mdl=Zoe&f=p'
 max_rank = 5
 
 versions = r'life|intens|zen'
@@ -44,6 +44,7 @@ def getAllLinks(url):
 
     return links
 
+# Get info for a given car
 def getInfoCar(link):
     soup = getSoupFromURL(link)
 
@@ -83,20 +84,42 @@ def getInfoCar(link):
         
         return car_info
 
+# Build dataframe with cars information
 def buildDataFrame(list_cars):
     df = pd.DataFrame(list_cars)
-    print(df.dtypes)
+    
+    return df
+
+# Get price from argus website
+def getInfoArgus(version):
+    url_argus = 'https://www.lacentrale.fr/cote-auto-renault-zoe-{}+charge+rapide-2013.html'.format(version)
+    
+    soup = getSoupFromURL(url_argus)
+    
+    if soup:
+        price = soup.find_all("span", class_='jsRefinedQuot')[0].text.strip().replace(' ', '')
+        return(float(price))
+
+# Add to the previous dataframe
+def buildArgusDataFrame(df):
+    argus = {version : getInfoArgus(version) for version in versions.replace('r', '').split('|')}
+    
+    df['argus'] = df['version'].map(argus)
     return df
 
 # Main
 if __name__ == "__main__":
     start_time = time.time()
     
-    links = getAllLinks(url_idf)
+    lists = [getAllLinks(url.format(region)) for region in regions]
+    links = list(itertools.chain.from_iterable(lists))
     
     list_cars = [getInfoCar(link) for link in links]
     
     df = buildDataFrame(list_cars)
+    
+    buildArgusDataFrame(df)
+    
     print(df)
 
     print("\nTime elapsed to get avg stargazer per user : {} s".format(time.time() - start_time))
